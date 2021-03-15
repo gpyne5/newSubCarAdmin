125de2f213559b0f32e22bbb3d7aee551ff85fa8
gpyne5 <w5000c2d26@gmail.com>
2021-03-15 17:58:23 +0900

    deleteメソッドを作成

--- lambda/delete/index.js
+++ lambda/delete/index.js
@@ -3,5 +3,18 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async event => {
 
+    console.log(typeof Number(event.queryStringParameters['id']))
     
+    const params = {
+        'TableName': 'Cars',
+        'Key': {
+            'car_id': Number(event.queryStringParameters['id'])
+        }
+    }
+    await dynamo.delete(params).promise();
+    
+    return {
+        statusCode: 200,
+
+    }
 }
